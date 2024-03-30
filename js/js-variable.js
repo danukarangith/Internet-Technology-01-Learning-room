236d9f9127485d00b4a96619c6b34fe4f04c54da
danukarangith <danukarangith456@gmail.com>
2024-03-30 15:03:39 +0530

    update

---
+++ js/js-variable.js
@@ -0,0 +1,17 @@
+// keyword in varibles defining in javascript
+// var,let,const
+
+let firstName = "danuka"
+let lastName = "perera"
+
+var age = 30;
+const salary =5000.00;
+console.log(firstName + "" + lastName)  
+console.log(firstName , lastName) 
+
+// important
+// 1.canot be a reserved word
+// 2.variable name should be a meaningful
+// 3.canot start with a number
+// 4.canot contain spaces or hypernet
+// 5.case sensitive
---
+++ js/primitive-data-types.js
@@ -0,0 +1,5 @@
+// there are 6 yypes in java script
+
+// 1.String
+let firstName = 'kamal'
+console.log('type of firstName :' , typeof firstName)
