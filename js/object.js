f42ad49967574e6ad653de6f5e20a328fb29b4a6
danukarangith <danukarangith456@gmail.com>
2024-03-30 16:06:54 +0530

    object create

---
+++ js/object.js
@@ -0,0 +1,21 @@
+let student ={}
+console.log('type of student:',typeof student)
+console.log('student :', student)
+
+
+
+student ={
+    firstName: 'danuka',
+    lastName: 'kavinda',
+    email:'danuka@gmail.com',
+    phoneNumber:'0771234567',
+    isRegitered:true,
+    age:13
+}
+console.log('student :', student)
+console.log('student :', student.email)
+console.log('student :', student['email'])
+
+student.age = 20;
+console.log('age :',student.age)
+console.log('age :',student)
