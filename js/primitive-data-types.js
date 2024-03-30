2ceb0d61ffc49369706dcac4b3ccf946be846a37
danukarangith <danukarangith456@gmail.com>
2024-03-30 15:30:05 +0530

    update

--- js/primitive-data-types.js
+++ js/primitive-data-types.js
@@ -3,3 +3,30 @@
 // 1.String
 let firstName = 'kamal'
 console.log('type of firstName :' , typeof firstName)
+
+// 2.number
+
+let age = 30;
+console.log('type of age :',typeof age)
+
+// 3.Boolean
+let isAdded = true
+console.log('type of isAdded :',typeof isAdded)
+
+
+// 4.undefined
+let address;
+console.log('type of address :',typeof address)
+
+let email;
+console.log('type of email :',typeof email)
+
+// 5.null represents the international absence of any  object value it is a primitive value is
+let phoneNumber = null
+console.log('type of email :',typeof phoneNumber)
+
+// 6.symbol
+let symbol =Symbol ("firstName")
+console.log('type of email :',typeof symbol)
+
+
