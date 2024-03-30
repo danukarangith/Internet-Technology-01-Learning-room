e3621cd98ecb8f8a1a6704fd558ec074969d7e7c
danukarangith <danukarangith456@gmail.com>
2024-03-30 15:43:12 +0530

    dynamic varibales

---
+++ js/dynamic-variable.js
@@ -0,0 +1,8 @@
+let testingvariable;
+console.log('type of testingvaribale :' , typeof testingvariable)
+
+testingvariable =true
+console.log('type of testingvaribale :' , typeof testingvariable)
+
+testingvariable =30;
+console.log('type of testingvaribale :' , typeof testingvariable)
