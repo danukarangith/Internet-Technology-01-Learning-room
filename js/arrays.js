65c054447d8a2d8b04889d972f9671be5d05ad80
danukarangith <danukarangith456@gmail.com>
2024-03-30 16:27:09 +0530

    array

---
+++ js/arrays.js
@@ -0,0 +1,20 @@
+let fruits =[]
+console.log('type of fruit :', typeof fruits)
+console.log('type of fruit :', typeof fruits)
+console.log('type of fruit :', fruits.length)
+
+ fruits =['apple','orange' ,'pineapp']
+ console.log('type of fruit :', typeof fruits)
+console.log('type of fruit :',  fruits)
+console.log('type of fruit :', fruits.length)
+
+console.log('type of fruit :', fruits[1])
+console.log('type of fruit :', fruits[4])
+fruits[2]= 'kesel';
+console.log('type of fruit :',  fruits)
+
+fruits[5]='woodapple'
+
+console.log('type of fruit :',  fruits)
+console.log('type of fruit :', fruits[4])
+
