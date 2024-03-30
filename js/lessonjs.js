8513122d3f525d69b3a4733063c2e2fb0f6c22f8
danukarangith <danukarangith456@gmail.com>
2024-03-30 14:28:55 +0530

    update

---
+++ js/lessonjs.js
@@ -0,0 +1,9 @@
+console.log("hello universe!");
+
+console.log(10+20)
+console.log(10/20)
+
+console.log("Add : 30");
+console.log("sub : -");
+console.log("devide : /");
+console.log("Multiple : *");
