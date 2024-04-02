b4c95b666c554fc185931a59a6d3d4e5665b8121
danukarangith <danukarangith456@gmail.com>
2024-04-02 21:30:11 +0530

    update

--- js/object.js
+++ js/object.js
@@ -1,24 +1,24 @@
-// let student ={}
-// console.log('type of student:',typeof student)
-// console.log('student :', student)
+let student ={}
+console.log('type of student:',typeof student)
+console.log('student :', student)
 
 
 
-// student ={
-//     firstName: 'danuka',
-//     lastName: 'kavinda',
-//     email:'danuka@gmail.com',
-//     phoneNumber:'0771234567',
-//     isRegitered:true,
-//     age:13
-// }
-// console.log('student :', student)
-// console.log('student :', student.email)
-// console.log('student :', student['email'])
+student ={
+    firstName: 'danuka',
+    lastName: 'kavinda',
+    email:'danuka@gmail.com',
+    phoneNumber:'0771234567',
+    isRegitered:true,
+    age:13
+}
+console.log('student :', student)
+console.log('student :', student.email)
+console.log('student :', student['email'])
 
-// student.age = 20;
-// console.log('age :',student.age)
-// console.log('age :',student)
+student.age = 20;
+console.log('age :',student.age)
+console.log('age :',student)
 
 let school ={}
 console.log('type of school :', typeof school)
