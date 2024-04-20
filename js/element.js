80f3201137d26430761bb0b1bbc8403a64782646
danukarangith <danukarangith456@gmail.com>
2024-04-20 16:06:44 +0530

    element js

---
+++ js/element.js
@@ -0,0 +1,9 @@
+console.log('Hey there')
+let calculateButton = document.getElementById('btnCalculate')
+console.log(calculateButton)
+
+
+// class selector
+
+let boxes=document.getElementsByClassName('box')
+console.log(calculateButton)
