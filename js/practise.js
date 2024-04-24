b2e9b9cd748203884a40243f9299088a17698004
danukarangith <danukarangith456@gmail.com>
2024-04-24 14:11:39 +0530

    varible use

--- js/practise.js
+++ js/practise.js
@@ -1,3 +1,6 @@
+ let clickbtn = document.getElementById("btn");
+ clickbtn.style.backgroundColor="red";
+  
  document.getElementById("btn").onclick=function(){
 
     document.getElementById("text").innerHTML="welcome danu";
