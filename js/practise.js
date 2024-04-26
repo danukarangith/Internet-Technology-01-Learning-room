f7e21374f70c00a922b40eb5c86120151276c4c0
danukarangith <danukarangith456@gmail.com>
2024-04-26 13:21:53 +0530

    formatter

--- js/practise.js
+++ js/practise.js
@@ -1,35 +1,26 @@
- let clickbtn = document.getElementById("btn");
- clickbtn.style.backgroundColor="red";
-  
- document.getElementById("btn").onclick=function(){
+let clickbtn = document.getElementById("btn");
+clickbtn.style.backgroundColor = "red";
 
-    document.getElementById("text").innerHTML=document.getElementById("text").innerHTML+ "danuka";
-    document.getElementById("text").style.color="red";
-    document.getElementById("text").style.border="2px solid black  "
-    
- }
+document.getElementById("btn").onclick = function () {
+  document.getElementById("text").innerHTML =
+    document.getElementById("text").innerHTML + "danuka";
+  document.getElementById("text").style.color = "red";
+  document.getElementById("text").style.border = "2px solid black  ";
+};
 
-  document.getElementById("btn2").onclick=function(){
-   document.getElementById("empty").innerHTML="lorem bn pisseks hsbsbsbuwsbu"
-  }
-   var x = 10;
-   x=25;
-  document.getElementById("btn3").onclick=function(){
+document.getElementById("btn2").onclick = function () {
+  document.getElementById("empty").innerHTML = "lorem bn pisseks hsbsbsbuwsbu";
+};
+var x = 10;
+x = 25;
+document.getElementById("btn3").onclick = function () {
+  alert(x);
+};
 
- alert(x)
-       
-   
+document.getElementById("update").onclick = function () {
+  var uptext = "";
 
-  }
+  uptext = document.getElementById("input").value;
 
-
-  document.getElementById("update").onclick=function(){
-
-   var uptext =""
-   uptext=document.getElementById("input").value;
-
-   alert(uptext);
-  }
-
-   
-
+  alert(uptext);
+};
