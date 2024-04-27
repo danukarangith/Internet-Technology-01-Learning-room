cb8a4bbfb8aad74c944d2c9b48b1ff931ed7b3fe
danukarangith <danukarangith456@gmail.com>
2024-04-27 10:56:28 +0530

    timers in js

---
+++ js/timeoutex.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 10; i++) {
+    setInterval(function() {
+        console.log(i);
+      }, 2000);
+     
+  }
+
+  
+
+ 
+
+
+ 
+
+  
---
+++ js/timers.js
@@ -0,0 +1,21 @@
+// 1.setTimeout
+let timeout=setTimeout(()=>{
+    console.log("hello")
+    
+},5000)
+clearTimeout(timeout)
+
+// 2.set interval
+
+ let interval =setInterval(() => {
+    console.log("hey ballo")
+    
+},1000);
+
+clearInterval(interval)
+
+let greet = function(){
+    console.log("hello world ")
+}
+ 
+
