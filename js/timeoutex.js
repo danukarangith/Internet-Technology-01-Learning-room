fd7c1115857c512002ce5130dba78250e4b274f1
danukarangith <danukarangith456@gmail.com>
2024-04-27 11:37:49 +0530

    timers exercise

--- js/timeoutex.js
+++ js/timeoutex.js
@@ -1,15 +1,32 @@
-for (let i = 1; i <= 10; i++) {
-    setInterval(function() {
-        console.log(i);
-      }, 2000);
+// let count = 0;
+// let interval =setInterval(() => {
+//     count++
+//     console.log(count)
+//     if(count==10){
+//         clearInterval(interval)
+//     }
+    
+// },2000);
+
+function print() {
+    let letter = "abcd";
      
-  }
-
   
-
- 
-
-
- 
-
+    let interval = setInterval(function() {
+      console.log(letter);
+      
+  
+      if ( letter ==clearInterval) {
+        clearInterval(interval);
+        
+      }
+    }, 500);  
+  
+    setTimeout(function() {
+      clearInterval(interval);
+      
+    }, 5000);  
+  }
+  
+  print();
   
