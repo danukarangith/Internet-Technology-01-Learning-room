b052b7394ba587a3d03aec317c1b048537b88b7c
danukarangith <danukarangith456@gmail.com>
2024-04-27 14:31:11 +0530

    applying style

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,20 @@
-let box = document.getElementById("box");
-console.log(box);
+// let box = document.getElementById("box");
+// console.log(box);
 
-console.log(typeof box.innerHTML);
+// console.log(typeof box.innerHTML);
 
-box.innerHTML = "hello world";
+// box.innerHTML = "hello world";
 
-box.innerHTML = "<p>dog</p>";
-// box.innerHTML = ' <img src="/assest/21a-400x400.jpg" alt="">';
+// box.innerHTML = "<p>dog</p>";
+// // box.innerHTML = ' <img src="/assest/21a-400x400.jpg" alt="">';
 
-console.log(box.innerHTML);
+// console.log(box.innerHTML);
+
+// console.log(box.textContent)
+// box.textContent='kusal perera'
+// console.log(box.textContent)
+// applying style
+
+let image=document.getElementById('img') ;
+image.style.height='100px'
+
