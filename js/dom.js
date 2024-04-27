4da8a1a1972e7f9b0587704f4f6a96d1d9132858
danukarangith <danukarangith456@gmail.com>
2024-04-27 14:56:21 +0530

    classlist

--- js/dom.js
+++ js/dom.js
@@ -1,20 +1,33 @@
-// let box = document.getElementById("box");
-// console.log(box);
+let box = document.getElementById("box");
+console.log(box);
 
-// console.log(typeof box.innerHTML);
+console.log(typeof box.innerHTML);
 
-// box.innerHTML = "hello world";
+box.innerHTML = "hello world";
 
-// box.innerHTML = "<p>dog</p>";
-// // box.innerHTML = ' <img src="/assest/21a-400x400.jpg" alt="">';
+box.innerHTML = "<p>dog</p>";
+// box.innerHTML = ' <img src="/assest/21a-400x400.jpg" alt="">';
 
-// console.log(box.innerHTML);
+console.log(box.innerHTML);
 
-// console.log(box.textContent)
-// box.textContent='kusal perera'
-// console.log(box.textContent)
-// applying style
+console.log(box.textContent)
+box.textContent='kusal perera'
+console.log(box.textContent)
+
+// 4.applying style
 
 let image=document.getElementById('img') ;
 image.style.height='100px'
 
+
+// 5.set Attribute
+
+image.setAttribute('src','/assest/429579098_2140284036307464_8313141474756476804_n.jpg')
+
+// 6.classlist
+
+console.log(box.classList);
+box.classList.add('abx')
+console.log(box.classList)
+
+box.classList.remove('abx')
