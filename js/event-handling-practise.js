19cae37c37feadfb91c90187386692547ed424a4
danukarangith <danukarangith456@gmail.com>
2024-05-10 13:44:26 +0530

    connection alerts

--- js/event-handling-practise.js
+++ js/event-handling-practise.js
@@ -1,15 +1,21 @@
 const btn =document.getElementById('btn');
 
 btn.onclick=function(){
-     console.log("hello");
+    //  console.log("hello");
 }
 btn.onclick=function(e){
-    console.log(e);
+    // console.log(e);
 }
 
 btn.addEventListener("click",function(){
-    console.log("hi 1")
+    // console.log("hi 1")
 })
 btn.addEventListener("click",function(){
-    console.log("hi 2")
+    // console.log("hi 2")
+})
+window.addEventListener("offline",function(){
+    alert("Your network is offline")
+})
+window.addEventListener("online",function(){
+    alert("your conection is back")
 })
